241293b76606e37005b6eab8eeeb14f82f8a3c7b
Kelly Powers <kpowers.nj@gmail.com>
2020-01-08 05:52:36 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -30,15 +30,15 @@ def reverse_each_word(string)
   new_array.join(" ")
 end
 
-def reverse_each_word(string) 
-  string_split = string.split(' ')
+# def reverse_each_word(string) 
+#   string_split = string.split(' ')
 
-  new_array = []
-  string_split.each do |w|
-    new_array << w.reverse()
-  end   
-  new_array.join(' ') 
-end  
+#   new_array = []
+#   string_split.each do |w|
+#     new_array << w.reverse()
+#   end   
+#   new_array.join(' ') 
+# end  
 
 reverse_each_word("Hello there, and how are you?")
   
